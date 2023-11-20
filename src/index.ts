b5ce7d0151c62488ba49bb8b508fd6d78f90cd99
David Park <jwpark511@gmail.com>
2023-11-19 17:44:59 -0800

    Add eslint

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import { env } from './config.js';
-import { AppContext } from './types';
+import type { AppContext } from './types';
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
